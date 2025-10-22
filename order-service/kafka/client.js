3eca391d1146c41bedbd00395fd9cc505caa3a05
Arjun Mepparambath <arjun.krishna.291297@gmail.com>
2025-10-22 11:44:38 +0530

    Fix file tracking and add shell scripts

--- order-service/kafka/client.js
+++
@@ -1,9 +0,0 @@
-import { Kafka } from "kafkajs";
-import dotenv from "dotenv";
-dotenv.config();
-const kafka = new Kafka({
-  clientId: "admin-client",
-  brokers: [process.env.KAFKA_BROKERS || "kafka:29092"],
-  retry: { initialRetryTime: 3000, retries: 10 }
-});
-export default kafka;
